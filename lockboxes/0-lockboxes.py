f0751195c0a90a966e90e03529969256ddf9af58
Fuad Khanaliyev <fuadkhanaliyev@Fuads-MacBook-Air.local>
2026-03-29 23:41:20 +0400

    dir

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-Məqsəd: Bütün qutuların açılıb-açılmadığını təyin edən metod
-"""
+"""0-lockboxes.py"""
 
 
 def canUnlockAll(boxes):
+    """Determines if all boxes can be unlocked."""
     if not boxes:
         return False
-    
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
     keys = boxes[0]
-    
     for key in keys:
         if key < n and not unlocked[key]:
             unlocked[key] = True
             keys.extend(boxes[key])
-            
     return all(unlocked)
